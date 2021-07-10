561fc3883cafef0d8610397928c2f707befe3f40
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-07-10 15:38:04 +0300

    Updates

---
+++ static/js/main.4b30bb3a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],[function(e,c){}],[[0,1]]]);
+//# sourceMappingURL=main.4b30bb3a.chunk.js.map
